9bbd01a5c5e725d79405958a26d33ea08f40a4a3
Neeraj Maurya <neerajxp@gmail.com>
2017-12-29 12:15:03 -0500

    Add project files.

---
+++ CoinTimeWebsite/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CoinTimeWebsite.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CoinTimeWebsite/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CoinTimeWebsite.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
